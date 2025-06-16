39ba1a57a1737a31695bd1e9de9597a7d08b3d55
Aishwarypandey30 <aishwarypandey30@gmail.com>
2025-06-16 15:26:52 +0530

    create user with token done

--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
+import { jwtCheck } from "../middleware/auth";
 
 const router = express.Router();
 
 // /api/my/user
-router.post("/", (req, res, next) => {
+router.post("/",jwtCheck, (req, res, next) => {
   MyUserController.createCurrentUser(req, res)
-})
+});
+router.put("/",(req,res,next) =>{
+  MyUserController.updateCurrentUser(req, res)
+});
 
 
 export default router;
