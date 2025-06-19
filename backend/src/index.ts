e63f8c5285e2cb5c266269af243214269ad9c51a
Aishwarypandey30 <aishwarypandey30@gmail.com>
2025-06-19 18:06:29 +0530

    Create my restaurant done , next is get my restaurant deatails

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,12 +3,20 @@ import cors from 'cors';
 import "dotenv/config";
 import mongoose from 'mongoose';
 import myUserRoute from "./routes/MyUserRoute"
+import { v2 as cloudinary } from "cloudinary"
+import myRestaurantRoute from "./routes/MyRestaurantRoute"
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
   console.log('Connected to MongoDB');
 }).catch((error) => {
   console.error('Error connecting to MongoDB:', error);
   process.exit(1);
+});
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const app = express();
@@ -20,6 +28,7 @@ app.get("/health",async (req: Request, res: Response)=>{
 })
 
 app.use("/api/my/user",myUserRoute);
+app.use("/api/my/restaurant",myRestaurantRoute);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT || 3000}`);
