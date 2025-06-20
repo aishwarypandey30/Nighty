c0544b71f6a98d113392a0beacbdabce7776197c
Aishwarypandey30 <aishwarypandey30@gmail.com>
2025-06-20 17:53:11 +0530

    search and sort done

---
+++ backend/src/routes/RestaurantRoute.ts
@@ -0,0 +1,19 @@
+import express , { Request, Response } from "express";
+import { param } from "express-validator";
+import RestaurantController from "../controllers/RestaurantController";
+
+const router = express.Router();
+
+router.get(
+  "/search/:city",
+  param("city")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("City paramenter must be a valid string"),
+    (req : Request,res : Response) => {
+    RestaurantController.searchRestaurant(req,res);
+  }
+);
+
+export default router;
