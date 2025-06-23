35ee2e21419e799ba1fe688d67fcb6ac0fc3d155
Aishwarypandey30 <aishwarypandey30@gmail.com>
2025-06-23 14:46:12 +0530

    checkout flow

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import myUserRoute from "./routes/MyUserRoute"
 import { v2 as cloudinary } from "cloudinary"
 import myRestaurantRoute from "./routes/MyRestaurantRoute"
 import RestaunrantRoute from "./routes/RestaurantRoute"
+import orderRoute from "./routes/OrderRoute";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
   console.log('Connected to MongoDB');
@@ -21,8 +22,11 @@ cloudinary.config({
 });
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+
+app.use("/api/order/checkout/webhook",express.raw({ type : "*/*" }));
+
+app.use(express.json());
 
 app.get("/health",async (req: Request, res: Response)=>{
   res.send({message : "health OK!"});
@@ -31,6 +35,7 @@ app.get("/health",async (req: Request, res: Response)=>{
 app.use("/api/my/user",myUserRoute);
 app.use("/api/my/restaurant",myRestaurantRoute);
 app.use("/api/restaurant",RestaunrantRoute);
+app.use("/api/order", orderRoute);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT || 3000}`);
--- backend/src/routes/RestaurantRoute.ts
+++ backend/src/routes/RestaurantRoute.ts
@@ -1,8 +1,19 @@
-import express , { Request, Response } from "express";
+import express ,{Request , Response} from "express";
 import { param } from "express-validator";
 import RestaurantController from "../controllers/RestaurantController";
 
 const router = express.Router();
+
+router.get(
+  "/:restaurantId",
+  param("restaurantId")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("RestaurantId paramenter must be a valid string"),
+    (req : Request,res : Response) => {
+    RestaurantController.getRestaurant(req,res);}
+);
 
 router.get(
   "/search/:city",
@@ -12,8 +23,7 @@ router.get(
     .notEmpty()
     .withMessage("City paramenter must be a valid string"),
     (req : Request,res : Response) => {
-    RestaurantController.searchRestaurant(req,res);
-  }
+    RestaurantController.searchRestaurant(req,res);}
 );
 
 export default router;
